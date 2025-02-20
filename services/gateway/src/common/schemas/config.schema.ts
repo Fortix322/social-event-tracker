66ee0874579fae2c350a11695c13c1d3f9c3909b
fortix <iserikov322@gmail.com>
2025-02-20 23:18:39 +0200

    [gateway]: added configurable body size limit

--- services/gateway/src/common/schemas/config.schema.ts
+++ services/gateway/src/common/schemas/config.schema.ts
@@ -4,7 +4,8 @@ export const envSchema = z.object({
     PORT: z.coerce.number(),
     NATS_SERVER: z.string().url(),
     NATS_STREAM_NAME_EVENT: z.string(),
-    NATS_SUBJECT_NAME_EVENT: z.string()
+    NATS_SUBJECT_NAME_EVENT: z.string(),
+    BODY_SIZE_LIMIT: z.string()
 });
 
 export type EnvVars = z.infer<typeof envSchema>;
--- services/gateway/src/main.ts
+++ services/gateway/src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EnvConfigService } from './config/config.service';
+import { urlencoded } from 'express';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(EnvConfigService);
   const port = configService.get('PORT');
+  const sizeLimit = configService.get('BODY_SIZE_LIMIT');
+
+  app.use(json({limit: sizeLimit}));
+  app.use(urlencoded({limit: sizeLimit, extended: true}));
+
+  
   await app.listen(port);
 }
 bootstrap();
